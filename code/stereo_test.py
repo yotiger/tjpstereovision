a21c38d9a42f415799d976255e5456a138d4571f
xtroce <xtroce@localhost>
2010-06-04 13:43:02 +0200

    stereo test for opencv 2.1

--- code/stereo_test.py
+++ code/stereo_test.py
@@ -1,23 +1,22 @@
+#!/usr/bin/python
+
 import sys
-from opencv import *
-from opencv.cv import *
-from opencv.cxcore import *
-from opencv.highgui import *
+import cv
 
 def findstereocorrespondence(image_left, image_right):
     # image_left and image_right are the input 8-bit single-channel images
     # from the left and the right cameras, respectively
     (r, c) = (image_left.rows, image_left.cols)
-    disparity_left = CreateMat(r, c, cv.CV_16S)
-    disparity_right = CreateMat(r, c, cv.CV_16S)
-    state = CreateStereoGCState(16, 2)
-    FindStereoCorrespondenceGC(image_left, image_right, disparity_left, disparity_right, state, 0)
+    disparity_left = cv.CreateMat(r, c, cv.CV_16S)
+    disparity_right = cv.CreateMat(r, c, cv.CV_16S)
+    state = cv.CreateStereoGCState(16, 2)
+    cv.FindStereoCorrespondenceGC(image_left, image_right, disparity_left, disparity_right, state, 0)
     return (disparity_left, disparity_right)
 
 
 if __name__ == '__main__':
 
-    (l, r) = [cvLoadImageM(f, CV_LOAD_IMAGE_GRAYSCALE) for f in sys.argv[1:]]
+    (l, r) = [cv.LoadImageM(f, cv.CV_LOAD_IMAGE_GRAYSCALE) for f in sys.argv[1:]]
 
     (disparity_left, disparity_right) = findstereocorrespondence(l, r)
 
