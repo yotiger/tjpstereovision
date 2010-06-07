ce0701935c805a5a32eaa9d76d1aa2e88466bebb
Sander Latour <Sander Latour@localhost>
2010-06-07 13:26:39 +0200

    Rewritten the copy of stereo_gc_test.py to a BM algorithm

--- code/stereo_bm_test.py
+++ code/stereo_bm_test.py
@@ -7,19 +7,15 @@ def findstereocorrespondence(image_left, image_right):
     # image_left and image_right are the input 8-bit single-channel images
     # from the left and the right cameras, respectively
     (r, c) = (image_left.rows, image_left.cols)
-    disparity_left = cv.CreateMat(r, c, cv.CV_16S)
-    disparity_right = cv.CreateMat(r, c, cv.CV_16S)
-    state = cv.CreateStereoGCState(16, 10)
-    cv.FindStereoCorrespondenceGC(image_left, image_right, disparity_left, disparity_right, state, 0)
-    return (disparity_left, disparity_right)
+    disparity = cv.CreateMat(r, c, cv.CV_16S)
+    state = cv.CreateStereoBMState(cv.CV_STEREO_BM_BASIC)
+    cv.FindStereoCorrespondenceGC(image_left, image_right, disparity, state)
+    return disparity
 
 
 if __name__ == '__main__':
-
     (l, r) = [cv.LoadImageM(f, cv.CV_LOAD_IMAGE_GRAYSCALE) for f in sys.argv[1:]]
-
-    (disparity_left, disparity_right) = findstereocorrespondence(l, r)
-
-    disparity_left_visual = cv.CreateMat(l.rows, l.cols, cv.CV_8U)
-    cv.ConvertScale(disparity_left, disparity_left_visual, -16)
-    cv.SaveImage("disparity.jpg", disparity_left_visual)
+    disparity = findstereocorrespondence(l, r)
+    disparity_visual = cv.CreateMat(l.rows, l.cols, cv.CV_8U)
+    cv.ConvertScale(disparity, disparity_visual, -16)
+    cv.SaveImage("disparityBM.jpg", disparity_visual)
