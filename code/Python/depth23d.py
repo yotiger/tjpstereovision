9c4ed3310b2a48672cca2a6324e26d163c1094c7
xtroce <xtroce@localhost>
2010-06-14 13:20:02 +0200

    better results for SGBM, depthimp (as of now in stereo_gc also, has to be removed again), first 3 points comments paper done

--- code/Python/depth23d.py
+++ code/Python/depth23d.py
@@ -6,9 +6,13 @@ from stereorectify import loadRectif
 
 disp = cv.LoadImageM(sys.argv[1], cv.CV_LOAD_IMAGE_GRAYSCALE)
 
-threedimg = cv.CreateMat(disp.rows, disp.cols, cv.CV_16S)
+threedimg = cv.CreateMat(disp.rows, disp.cols, cv.CV_16SC1)
 
-(R1, R2, P1, P2, Q) = loadRectif()
+(R1, R2, P1, P2, Q) = loadRectif("../../dataset/owndataset1/rect")
+
+disp.type = 3
+print Q.type
+print threedimg.type
 
 cv.ReprojectImageTo3D(disp, threedimg, Q)
 
