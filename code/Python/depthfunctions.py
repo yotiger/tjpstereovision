a4af13363c2293b532f70500951e5b4e27db97f5
Martijn van der Veen <Martijn van der Veen@localhost>
2010-06-21 15:39:40 +0200

    Added testscripts for using C++ in Python, edited sgbm c++ script

---
+++ code/Python/depthfunctions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Functions for depthmaps
+# University of Amsterdam
+# Stereo Vision Project, June 2010
+
+import cv
+
+
+# removeBackground: remove all pixels with value's less than treshold
+# todo: or replace with pixel from other image
+def removeBackground(image, depthmap, threshold=1.6e-304, background=None):
+  result = cv.CreateMat(image.height, image.width, image.type)
+
+  # do a lot of conversions to use cv.Threshold and get 3C image back
+  depthmap8u = cv.CreateMat(depthmap.height, depthmap.width, cv.CV_8UC1)
+  cv.Convert(depthmap, depthmap8u)
+  
+  depthmap_binary = cv.CreateMat(depthmap8u.height, depthmap8u.width, \
+                                 depthmap8u.type)
+  cv.Threshold(depthmap8u, depthmap_binary, threshold, 1, cv.CV_THRESH_BINARY)
+
+  depthmap_binary_16u = cv.CreateMat(depthmap.height, depthmap.width, \
+                                     cv.CV_16UC1)
+  depthmap_binary_c3  = cv.CreateMat(depthmap.height, depthmap.width, \
+                                     cv.CV_16UC3)
+  cv.Convert(depthmap_binary, depthmap_binary_16u)                    
+  # make 3-channel image from 1-channel image
+  cv.Merge(depthmap_binary_16u, depthmap_binary_16u, \
+           depthmap_binary_16u, None, \
+           depthmap_binary_c3)
+  # multiply binary image with image (black out pixels far away)
+  cv.Mul(image, depthmap_binary_c3, result)
+
+  return result
+
---
+++ code/scripts/reloadcam.sh
@@ -0,0 +1,8 @@
+sudo modprobe -r uvcvideo
+sudo modprobe -r v4l2_common
+sudo modprobe -r videodev
+sudo modprobe -r v4l1_compat
+sudo modprobe uvcvideo
+sudo modprobe videodev
+sudo modprobe v4l2_common
+sudo modprobe v4l1_compat
