2f297853c6f1a8130d04b7c580bc082723514765
Martijn van der Veen <Martijn van der Veen@localhost>
2010-06-23 21:49:30 +0200

    Added SGBM and some extra options to depthmap_live, made remove-bg with alternative background, made masterscript for calibration and rectification, updated presentation

--- code/Python/depthfunctions.py
+++ code/Python/depthfunctions.py
@@ -14,10 +14,11 @@ def removeBackground(image, depthmap, threshold=180, background=None):
   image    - image to remove background from
   depthmap - depthmap of image
   threshold - threshold value
-  background - DOE ER IETS MEE MARTIJN!
+  background - if not None (but matrix), then use as background
   """
   
   result = cv.CreateMat(image.height, image.width, image.type)
+
 
   # do a lot of conversions to use cv.Threshold and get 3C image back
   depthmap8u = cv.CreateMat(depthmap.height, depthmap.width, cv.CV_8UC1)
@@ -38,5 +39,24 @@ def removeBackground(image, depthmap, threshold=180, background=None):
            depthmap_binary_c3)
   # multiply binary image with image (black out pixels far away)
   cv.Mul(image, depthmap_binary_c3, result)
+
+  # when alternative background is defined, set it for the black pixels
+  if background != None:
+    result_bg = cv.CreateMat(image.height, image.width, background.type)
+    depthmap_binary_inv = cv.CloneMat(depthmap_binary)
+    cv.Threshold(depthmap8u, depthmap_binary_inv, threshold, 1, cv.CV_THRESH_BINARY_INV)
+    #cv.Not(depthmap_binary_c3, depthmap_binary_inv)
+    depthmap_binary_inv_16u = cv.CreateMat(depthmap.height, depthmap.width, \
+                                           cv.CV_16UC1)
+    depthmap_binary_inv_c3  = cv.CreateMat(depthmap.height, depthmap.width, \
+                                           cv.CV_16UC3)
+    cv.Convert(depthmap_binary_inv, depthmap_binary_inv_16u)
+    # make 3-channel image from 1-channel image
+    cv.Merge(depthmap_binary_inv_16u, depthmap_binary_inv_16u, \
+             depthmap_binary_inv_16u, None, \
+             depthmap_binary_inv_c3)
+    cv.Mul(background, depthmap_binary_inv_c3, result_bg)
+    new_result = cv.CloneMat(result)
+    cv.Add(new_result, result_bg, result)
 
   return result
--- code/Python/depthmap_live.py
+++ code/Python/depthmap_live.py
@@ -5,6 +5,7 @@
 
 import cv
 import sys
+import os
 from stereoheader import *
 from stereo_bm_test import *
 
@@ -14,10 +15,35 @@ if __name__ == "__main__":
   CAM_RIGHT = 2
 
   # parse commandline args
-  if len(sys.argv) > 1 and sys.argv[1] == "remove-bg":
-    remove_background = True
-  else:
-    remove_background = False
+  remove_background = False
+  algoritm = "bm"
+  sgbm_path = "../C++/sgbm ../Python/{0} ../Python/{1}"
+  for i in range(1, len(sys.argv)):
+    if sys.argv[i][:11] == "--remove-bg":
+      remove_background = True
+      background_threshold = 250
+      if len(sys.argv[i]) > 11:
+        background_image_old = cv.LoadImageM(sys.argv[i][12:])
+        background_image = cv.CreateMat(480, 640, 18)
+        cv.Convert(background_image_old, background_image)
+      else:
+        background_image = None
+    elif sys.argv[i] == "--sgbm":
+      algoritm = "sgbm"
+      background_threshold = 70
+    elif sys.argv[i] == "--rectify-only":
+      algoritm = "rectify-only"
+    elif sys.argv[i] == "--webcamviewer":
+      algoritm = "none"
+
+  # usage message
+  print("Usage: " + sys.argv[0] + "[args]")
+  print(" Possible arguments:")
+  print("  --webcamviewer: show original images (don't do algoritms)")
+  print("  --rectify-only: show rectified images")
+  print("  --sgbm:         use sgbm instead of bm")
+  print("  --remove-bg:    show original image for front object pixels")
+
 
   # init vars
   capture_left  = cv.CaptureFromCAM(CAM_LEFT)
@@ -33,8 +59,24 @@ if __name__ == "__main__":
   while 1:
 
     # get frames
-    frame_left_col  = cv.QueryFrame(capture_left)
-    frame_right_col = cv.QueryFrame(capture_right)
+    cv.GrabFrame(capture_left)
+    cv.GrabFrame(capture_right)
+
+    # i know, this is bad programming,
+    # but seems to be the only way to drop 5 times the buffer
+    # (to get live images, even when framerate drops to 1fps)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_left_col  = cv.RetrieveFrame(capture_left)
+    frame_right_col = cv.RetrieveFrame(capture_right)
+    frame_right_col = cv.RetrieveFrame(capture_right)
+    frame_right_col = cv.RetrieveFrame(capture_right)
+    frame_right_col = cv.RetrieveFrame(capture_right)
+    frame_right_col = cv.RetrieveFrame(capture_right)
+    frame_right_col = cv.RetrieveFrame(capture_right)
 
     frame_left  = cv.CreateImage( (frame_left_col.width, frame_left_col.height), \
                           frame_left_col.depth, 1)
@@ -53,43 +95,93 @@ if __name__ == "__main__":
     cv.Convert(frame_right_col, framem_right_col)
 
 
-    # rectify frames
-    rectim_left, rectim_right = rectifyImages(framem_left, framem_right, saveImages = False)
-    
-    rectim8uc1_left  = cv.CreateMat(480, 640, cv.CV_8UC1)
-    rectim8uc1_right = cv.CreateMat(480, 640, cv.CV_8UC1)
-    #cv.Cvt(rectim_left,  rectim8uc1_left)
-    cv.Convert(rectim_left, rectim8uc1_left)
-    cv.Convert(rectim_right, rectim8uc1_right)
-    
-    depthmat = findstereocorrespondence(rectim8uc1_left, rectim8uc1_right)
+    if algoritm == "none":
 
-    
+      # webcamviewer: show original images
+      cv.ShowImage("leftimage", frame_left_col)
+      cv.ShowImage("rightimage", frame_right_col)
 
-    # depthmat:
-    depthimage  = cv.CreateImage( (framem_left.width, framem_left.height), \
-                          frame_left.depth, 1)
-    cv.Convert(depthmat, depthimage)
+    else:
 
-    # show image
-    cv.ShowImage("leftimage", depthimage)
+      # rectify frames
+      rectim_left_col, rectim_right_col = rectifyImages(framem_left_col, framem_right_col, saveImages = False)
+      
+      rectim_left  = cv.CreateMat(480, 640, cv.CV_16UC1)
+      rectim_right = cv.CreateMat(480, 640, cv.CV_16UC1)
+
+      cv.CvtColor(rectim_left_col,  rectim_left,  cv.CV_BGR2GRAY)
+      cv.CvtColor(rectim_right_col, rectim_right, cv.CV_BGR2GRAY)
+      rectim8uc1_left  = cv.CreateMat(480, 640, cv.CV_8UC1)
+      rectim8uc1_right = cv.CreateMat(480, 640, cv.CV_8UC1)
+      cv.Convert(rectim_left, rectim8uc1_left)
+      cv.Convert(rectim_right, rectim8uc1_right)
+
+      if algoritm == "rectify-only":
+        
+        # show rectified images
+        recti_left  = cv.CreateImage( (rectim_left_col.width, rectim_left_col.height), \
+                                      frame_left_col.depth, 3)
+        recti_right = cv.CreateImage( (framem_left_col.width, framem_left_col.height), \
+                                      frame_left_col.depth, 3)
+        cv.Convert(rectim_left_col,  recti_left)
+        cv.Convert(rectim_right_col, recti_right)
+
+        cv.ShowImage("leftimage", recti_left)
+        cv.ShowImage("rightimage", recti_right)
+
+      else:
+      
+        # first, do algoritm
+        if algoritm == "bm":
+
+          # use BlockMatching algoritm
+          depthmat = findstereocorrespondence(rectim8uc1_left, rectim8uc1_right)
+   
+          # depthmat:
+          depthimage  = cv.CreateImage( (framem_left.width, framem_left.height), \
+                                         frame_left.depth, 1)
+          cv.Convert(depthmat, depthimage)
+  
+        elif algoritm == "sgbm":
+
+          imgurl_left  = "sgbm_left.jpg"
+          imgurl_right = "sgbm_right.jpg"
+          cv.SaveImage(imgurl_left,  rectim8uc1_left)
+          cv.SaveImage(imgurl_right, rectim8uc1_right)
+
+          os.system(sgbm_path.format(imgurl_left, imgurl_right))
+
+          depthmat = cv.LoadImageM("sgbm.jpg", iscolor=cv.CV_LOAD_IMAGE_GRAYSCALE)
+          depthimage  = cv.CreateImage( (framem_left.width, framem_left.height), \
+                                         frame_left.depth, 1)
+          cv.Convert(depthmat, depthimage)
+
+        else:
+
+          print("Algoritm not implemented yet: " + algoritm)
+          sys.exit()
 
 
-    # Extra functions
-    # no-background-map:
-    if remove_background:
-      foo = removeBackground(framem_left_col, depthmat)
-      bar = cv.CreateImage( (frame_left_col.width, frame_left_col.height), \
-                            frame_left_col.depth, frame_left_col.nChannels)
-      cv.Convert(foo, bar)
-      cv.ShowImage("rightimage", bar)
+        # second, show depthmap
+        cv.ShowImage("leftimage", depthimage)
 
-      #foo2 = removeBackground(framem_right_col, depthmat)
-      #bar2 = cv.CreateImage( (frame_left_col.width, frame_left_col.height), \
-      #                      frame_left_col.depth, frame_left_col.nChannels)
-      #cv.Convert(foo2, bar2)
-      #cv.ShowImage("rightimage", bar2)
 
+        # third, do extra filters / functions
+        # and if they exist, show as second image
+          
+        # no-background-map:
+        if remove_background:
+          
+          foom = removeBackground(rectim_left_col, depthmat, threshold=background_threshold, background=background_image)
+          foo  = cv.CreateImage( (frame_left_col.width, frame_left_col.height), \
+                                  frame_left_col.depth, frame_left_col.nChannels)
+          cv.Convert(foom, foo)
+          # show image
+          cv.ShowImage("rightimage", foo)
+
+
+  
+  
     # check for exit
     k = cv.WaitKey(33)
     if k == 0x1b:
